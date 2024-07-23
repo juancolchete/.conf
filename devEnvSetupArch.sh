bb024c726fdf548dfbf5fe18702a881771b1bc82
juancolchete <juanudkp@gmail.com>
2024-07-23 21:08:35 +0000

    fix: not espacapign $

--- devEnvSetupArch.sh
+++ devEnvSetupArch.sh
@@ -16,11 +16,15 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 echo /usr/local/bin/fish | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/fish
 LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
-echo source "$HOME/.cargo/env.fish" >> ~~/.config/fish/config.fish/.config/fish/config.fish
-echo export PATH="$HOME/.local/bin:$PATH" >> ~/.config/fish/config.fish
-echo export PATH="$HOME/.local/share/solana/install/active_release/bin:$PATH" >> ~/.config/fish/config.fish
+echo source "\$HOME/.cargo/env.fish" >> ~/.config/fish/config.fish
+echo export PATH="\$HOME/.local/bin:\$PATH" >> ~/.config/fish/config.fish
+echo export PATH="\$HOME/.local/share/solana/install/active_release/bin:\$PATH" >> ~/.config/fish/config.fish
 echo source ~/.asdf/asdf.fish >> ~/.config/fish/config.fish
-echo export PATH="$HOME/.avm/bin:$PATH" >> ~/.config/fish/config.fish
+echo export PATH="\$HOME/.avm/bin:\$PATH" >> ~/.config/fish/config.fish
+echo export PATH="\$HOME/.local/bin:\$PATH" >> ~/.config/fish/config.fish
+echo export PATH="\$HOME/.local/share/solana/install/active_release/bin:\$PATH" >> ~/.config/fish/config.fish
+echo source ~/.asdf/asdf.fish >> ~/.config/fish/config.fish
+echo export PATH="\$HOME/.avm/bin:\$PATH" >> ~/.config/fish/config.fish
 source ~/.config/fish/config.fish
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 corepack enable
