e1731e27aace4a503d4fecb3484dfc8c3ec9e509
juancolchete <juanudkp@gmail.com>
2024-07-23 15:46:51 +0000

    fix: using asdf before its installation

--- devEnvSetupArch.sh
+++ devEnvSetupArch.sh
@@ -1,10 +1,7 @@
 sudo pacman -S docker-buildx docker neovim git make fish ripgrep python-pynvim
 sh -c "$(curl -sSfL https://release.solana.com/v1.18.18/install)"
-asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-corepack enable
-asdf reshim nodejs
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 echo /usr/local/bin/fish | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/fish
 LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
@@ -15,6 +12,9 @@ echo export PATH="$HOME/.local/share/solana/install/active_release/bin:$PATH" >>
 echo source ~/.asdf/asdf.fish >> ~/.config/fish/config.fish
 echo export PATH="$HOME/.avm/bin:$PATH" >> ~/.config/fish/config.fish
 source ~/.config/fish/config.fish
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+corepack enable
+asdf reshim nodejs
 echo "git username:"
 read name
 echo "git email:"
