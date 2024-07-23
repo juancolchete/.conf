516b1f56606b77aefa65d6eb3cc4c06d7389f1a7
juancolchete <juanudkp@gmail.com>
2024-07-23 22:38:50 +0000

    chore: config

---
+++ config.fish
@@ -0,0 +1,5 @@
+echo source "\$HOME/.cargo/env.fish" >> ~/.config/fish/config.fish
+echo export PATH="\$HOME/.local/bin:\$PATH" >> ~/.config/fish/config.fish
+echo source ~/.asdf/asdf.fish >> ~/.config/fish/config.fish
+echo export PATH="\$HOME/.local/share/solana/install/active_release/bin:\$PATH" >> ~/.config/fish/config.fish
+echo export PATH="\$HOME/.avm/bin:\$PATH" >> ~/.config/fish/config.fish
