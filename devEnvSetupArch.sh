ad9d5ea25273e51173d962f9c0dc6294e9fa87f6
juancolchete <juanudkp@gmail.com>
2024-07-23 22:05:53 +0000

    chore: remove fish part

--- devEnvSetupArch.sh
+++ devEnvSetupArch.sh
@@ -18,14 +18,4 @@ chsh -s /usr/local/bin/fish
 rm -rf /home/juanc/.local/share/lunarvim.old
 rm ~/.config/fish/config.fish
 LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
-source $HOME/.cargo/env.fish
-echo export PATH="\$HOME/.local/bin:\$PATH" >> ~/.config/fish/config.fish
-source ~/.asdf/asdf.fish
-echo export PATH="\$HOME/.local/share/solana/install/active_release/bin:\$PATH" >> ~/.config/fish/config.fish
-echo export PATH="\$HOME/.avm/bin:\$PATH" >> ~/.config/fish/config.fish
-source ~/.config/fish/config.fish
-asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-corepack enable
-asdf reshim nodejs
-rm ~/.config/lvim 
-git clone git@github.com:juancolchete/LunarVimLinux.git ~/.config/lvim
+
---
+++ devEnvSetupArchFish.sh
@@ -0,0 +1,11 @@
+source "\$HOME/.cargo/env.fish" >> ~/.config/fish/config.fish
+echo export PATH="\$HOME/.local/bin:\$PATH" >> ~/.config/fish/config.fish
+echo source ~/.asdf/asdf.fish >> ~/.config/fish/config.fish
+echo export PATH="\$HOME/.local/share/solana/install/active_release/bin:\$PATH" >> ~/.config/fish/config.fish
+echo export PATH="\$HOME/.avm/bin:\$PATH" >> ~/.config/fish/config.fish
+source ~/.config/fish/config.fish
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+corepack enable
+asdf reshim nodejs
+rm ~/.config/lvim 
+git clone git@github.com:juancolchete/LunarVimLinux.git ~/.config/lvim
