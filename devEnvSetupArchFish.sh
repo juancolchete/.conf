a4f29376fd82049ea7353e6b5209729842beae1a
juancolchete <juanudkp@gmail.com>
2024-07-23 22:41:45 +0000

    chore? configure to run on fish

--- devEnvSetupArchFish.sh
+++ devEnvSetupArchFish.sh
@@ -1,6 +1,3 @@
-echo source "\$HOME/.cargo/env.fish" >> ~/.config/fish/config.fish
-echo export PATH="\$HOME/.local/bin:\$PATH" >> ~/.config/fish/config.fish
-echo source ~/.asdf/asdf.fish >> ~/.config/fish/config.fish
-echo export PATH="\$HOME/.local/share/solana/install/active_release/bin:\$PATH" >> ~/.config/fish/config.fish
-echo export PATH="\$HOME/.avm/bin:\$PATH" >> ~/.config/fish/config.fish
-
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+corepack enable
+asdf reshim nodejs
