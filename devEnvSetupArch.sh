56731a5facf4e6f886ee64b2d3cd0b7f76d9cbf8
juancolchete <juanudkp@gmail.com>
2024-07-23 22:18:04 +0000

    chore fix

--- devEnvSetupArch.sh
+++ devEnvSetupArch.sh
@@ -18,7 +18,6 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 echo /usr/local/bin/fish | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/fish
 rm -rf /home/juanc/.local/share/lunarvim.old
-rm ~/.config/fish/config.fish
 LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
 rm -rf ~/.config/lvim 
 git clone git@github.com:juancolchete/LunarVimLinux.git ~/.config/lvim
--- devEnvSetupArchFish.sh
+++ devEnvSetupArchFish.sh
@@ -1,4 +1,5 @@
-source "\$HOME/.cargo/env.fish" >> ~/.config/fish/config.fish
+rm ~/.config/fish/config.fish
+echo source "\$HOME/.cargo/env.fish" >> ~/.config/fish/config.fish
 echo export PATH="\$HOME/.local/bin:\$PATH" >> ~/.config/fish/config.fish
 echo source ~/.asdf/asdf.fish >> ~/.config/fish/config.fish
 echo export PATH="\$HOME/.local/share/solana/install/active_release/bin:\$PATH" >> ~/.config/fish/config.fish
